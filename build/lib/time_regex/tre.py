a3e7433d4c0ed86fd9bb80a319c6c8a7056afcd2
Dermot Treanor <dermot.treanor1@google.com>
2025-08-09 14:09:05 +0100

    turned it into a package... a lot of stuff probably should be in gitignore??

---
+++ build/lib/time_regex/tre.py
@@ -0,0 +1,13 @@
+import re
+import time
+
+def show_combined_docs():
+    """Show the documentation for both the 're' and 'time' module."""
+    print("\033[31m" + re.__doc__ + "\033[0m")
+    print("\033[32m" + time.__doc__ + "\033[0m")
+
+def main():
+    print("Combining the 're' module with the 'time' module. Here are their descriptions.")
+
+if __name__ == '__main__':
+   show_combined_docs() 

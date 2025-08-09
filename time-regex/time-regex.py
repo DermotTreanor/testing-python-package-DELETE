ec1e8e5312947fe6b6bc205478940527e95bb0da
Dermot Treanor <dermot.treanor1@google.com>
2025-08-09 12:41:44 +0100

    changed the time module color to green

--- time-regex/time-regex.py
+++ time-regex/time-regex.py
@@ -4,10 +4,10 @@ import time
 def show_combined_docs():
     """Show the documentation for both the 're' and 'time' module."""
     print("\033[31m" + re.__doc__ + "\033[0m")
-    print("\033[34m" + time.__doc__ + "\033[0m")
+    print("\033[32m" + time.__doc__ + "\033[0m")
 
 def main():
     print("Combining the 're' module with the 'time' module. Here are their descriptions.")
 
 if __name__ == '__main__':
-    main()
+   show_combined_docs() 
